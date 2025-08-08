c4740857a877f9fa727464dd43f3383fd8ccf0f1
Elias2ano <elias.cardoso.da.silva.junior17@gmail.com>
2025-08-08 12:23:35 -0300

    Create exercicio_6.java

---
+++ exercicio_6.java
@@ -0,0 +1,107 @@
+
+import java.util.Scanner;
+
+public class exercicio_6 {
+    final static Scanner LER = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    int num1 = 0;
+    System.out.println("Digite o primeiro valor:");
+    num1 = LER.nextInt();
+
+    int num2 = 0;
+    System.out.println("Digite o segundo valor:");
+    num2 = LER.nextInt();
+
+    int num3 = 0;
+    System.out.println("Digite o terceiro valor:");
+    num3 = LER.nextInt();
+
+    int primeiroValor = 0;
+   
+    int segundoValor = 0;
+
+    int terceiroValor = 0;
+
+    if (num1 > num2 && num2 > num3) {
+        primeiroValor = num1;
+        segundoValor = num2;
+        terceiroValor = num3;
+    }else{
+        if (num1 > num2 && num2 < num3) {
+        primeiroValor = num1;
+        segundoValor = num3;
+        terceiroValor = num2;
+        }else{
+            if (num1 < num2 && num2 < num3) {
+            primeiroValor = num3;
+            segundoValor = num2;
+            terceiroValor = num1;
+            }else{
+                if (num1 > num2 && num1 < num3) {
+                primeiroValor = num3;
+                segundoValor = num1;
+                terceiroValor = num2;
+                }else{
+                    if (num1 < num2 && num1 > num3) {
+                    primeiroValor = num2;
+                    segundoValor = num1;
+                    terceiroValor = num3;
+                    }else{
+                        if (num3 < num2 && num1 < num3) 
+                        primeiroValor = num2;
+                        segundoValor = num3;
+                        terceiroValor = num1;
+                    }
+                }
+            }
+        }
+    }
+
+    System.out.println("Valores em ordem crescente: " + terceiroValor + ", " + segundoValor + ", " + primeiroValor );
+
+    if (num1 > num2 && num2 > num3) {
+        primeiroValor = num3;
+        segundoValor = num2;
+        terceiroValor = num1;
+    }else{
+        if (num1 > num2 && num2 < num3) {
+        primeiroValor = num2;
+        segundoValor = num3;
+        terceiroValor = num1;
+        }else{
+            if (num1 < num2 && num2 < num3) {
+            primeiroValor = num1;
+            segundoValor = num2;
+            terceiroValor = num3;
+            }else{
+                if (num1 > num2 && num1 < num3) {
+                primeiroValor = num2;
+                segundoValor = num1;
+                terceiroValor = num3;
+                }else{
+                    if (num1 < num2 && num1 > num3) {
+                    primeiroValor = num3;
+                    segundoValor = num1;
+                    terceiroValor = num2;
+                    }else{
+                        if (num3 < num2 && num1 < num3) 
+                        primeiroValor = num1;
+                        segundoValor = num3;
+                        terceiroValor = num2;
+                    }
+                }
+            }
+        }
+    }
+
+    System.out.println("Valores em ordem decrescente: " + terceiroValor + ", " + segundoValor + ", " + primeiroValor + "   *extra.");
+
+    while(true) {
+        if(primeiroValor != segundoValor || segundoValor != terceiroValor)
+            break;
+    }
+
+    }
+    
+}
