8521e3924b2134da3b360bfb7f7cd553a9c4dda7
EliasCardoso-2ano <elias.cardoso.da.silva.junior17@gmail.com>
2025-08-26 17:25:34 -0300

    .

--- exercicio_13.java
+++ exercicio_13.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class exercicio_13 {
      final static Scanner LER = new Scanner(System.in);
     public static void main(String[] args) {
-        
-        double vaiavel = 0;
+
+        int valor = 0;
+        System.out.println("Digite um valor: ");
+        valor = LER.nextInt();
+
+        int cotador = 0;
 
     }
 }
---
+++ exercico_16.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class exercico_16 {
+    public static Scanner LER = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        float saldoCliente = 0f;
+        System.out.println("Digete o seu saldo : ");
+        saldoCliente = LER.nextInt();
+
+        float saldoMédio = 0f;
+        saldoMédio =( saldoCliente / 2 );
+        System.out.println("Seu saldo médio é de: " + saldoMédio);
+
+
+        if (saldoMédio == 0 && <= 200) {
+            System.out.println("Vocé não tem nenhum crédito");
+        }else{
+        if (saldoMédio == 201 && <= 400) {
+            System.out.println("Seu percentual é de 20%");                
+        }else{
+        if (saldoMédio == 401 && <= 600) {
+            System.out.println("Seu percentual é de 30%");
+        }else{
+        if (saldoMédio > 600 ) {
+            System.out.println("Seu percentual é de 40%");
+            }
+            }
+            }
+        }
+
+
+
+
+
+    }
+}
